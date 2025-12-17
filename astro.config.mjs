1531dd3e913a54c6d45516d3b42c56fffcdd187c
Kumar Sahil <krsahil8825@gmail.com>
2025-12-18 00:13:14 +0530

    Add sitemap integration and update configuration files
    - Integrated @astrojs/sitemap into astro.config.mjs for improved SEO.
    - Updated package.json and package-lock.json to include @astrojs/sitemap dependency.
    - Added robots.txt file to define crawling rules and specify sitemap location.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,21 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
-import tailwindcss from '@tailwindcss/vite';
+import sitemap from "@astrojs/sitemap";
+
+import tailwindcss from "@tailwindcss/vite";
+
+import { SITE } from "./src/config/site";
 
 // https://astro.build/config
 export default defineConfig({
-  vite: {
-    plugins: [tailwindcss()]
-  }
+    site: SITE.url,
+    integrations: [
+        sitemap({
+            filter: (page) => !page.includes("404"),
+        }),
+    ],
+    vite: {
+        plugins: [tailwindcss()],
+    },
 });
